a853ec4dcbfbcf65d38e6bfd9eefdcd106647150
Jon Edwards <jkedwards@me.com>
2019-02-09 16:43:20 -0800

    change build tooling and add build support
    remove rollup and add Fuse-box
    add start script and remove deprecated scripts
    add Prettier and config
    change tsconfig and tslint configuration
    add entry index.html and index.ts
    add VS Code settings
    lint src with new settings
    add a defaultMachine in the form of a text file

---
+++ fuse.js
@@ -0,0 +1,32 @@
+const {
+  FuseBox,
+  RawPlugin,
+  StyledComponentsPlugin,
+  WebIndexPlugin,
+} = require('fuse-box')
+
+const fuse = FuseBox.init({
+  homeDir: 'src',
+  output: 'dist/$name.js',
+  target: 'browser@es6',
+  sourceMaps: true,
+  useTypescriptCompiler: true,
+  debug: true,
+  plugins: [
+    RawPlugin(['*.js.txt']),
+    StyledComponentsPlugin(),
+    WebIndexPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+})
+
+fuse.dev()
+
+fuse
+  .bundle('app')
+  .watch()
+  .hmr()
+  .instructions('> index.ts +util')
+
+fuse.run()
